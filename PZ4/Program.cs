273b8effbed11e0bdb3bcae3688ac45d8f821c29
Nikita <nrffg@inbox.ru>
2022-03-10 21:18:24 +0700

    first commit

---
+++ PZ4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taburet first = new Taburet();
+            first.SetHeight(22);
+            first.PrintHeight();
+            first.SetLeg(2);
+            first.PrintLeg();
+            first.GetMaterial("Дерево");
+            first.PrintMaterial();
+            Console.ReadLine();
+        }
+    }
+}
